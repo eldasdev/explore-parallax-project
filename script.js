5fcee5016c47c0443939032e822c9b7183f12ac9
EldasDEV | Shavkat Toshpulotov <73429646+eldasdev@users.noreply.github.com>
2021-05-19 09:21:33 +0500

    Updated script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
+
+timeline
+  .to('.rock', 3, {y: -300})
+  .to('.girl', 3, {y: -200},'-=3')
+  .to('.bg1', {y: -50}, {y: 0, duration: 3}, "-=3")
+  .to('.content', 3, {top: '0%'}, "-=3")
+  .fromTo('.content-images', 3, {opacity: 0}, {opacity: 1, duration: 3})
+  .fromTo('.text', 3, {opacity: 0}, {opacity: 1, duration: 3});
+
+  let scene = new ScrollMagic.Scene({
+    triggerElement: "section",
+    duration: "300%",
+    triggerHook: 0,
+  })
+  
+  .setTween(timeline)
+  .setPin("section")
+  .addTo(controller)
